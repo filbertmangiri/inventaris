8c4d40996e6363db05f7788b1ce9408ce916af86
Filbert Mangiri <filbertmangiri12345@gmail.com>
2024-12-30 01:49:35 +0700

    Initial commit

---
+++ resources/views/layouts/landing/hero.blade.php
@@ -0,0 +1,16 @@
+<div class="text-white bg-sky-800 w-full py-10">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
+            <div class="md:col-span-7 flex flex-col gap-4">
+                <h1 class="text-center md:text-start text-4xl font-bold md:text-6xl text-gray-100">
+                    StockServe
+                </h1>
+                <p class="text-sm md:text-lg text-center md:text-start text-gray-300">
+                    StockServe menyediakan informasi bahan baku catering secara lengkap dan memungkinkan permintaan dilakukan kapan saja, di mana saja.                </p>
+            </div>
+            <div class="hidden md:flex md:col-span-5">
+                <img src="{{ asset('gudang.svg') }}" class="w-full" />
+            </div>
+        </div>
+    </div>
+</div>
